53c7769091fb6579dc6135dd03ff58f0c5f1d4c4
fr4nbtt <batet.francisco@gmail.com>
2026-07-18 19:22:03 -0300

    Create Greeting component and render it in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg';
 import viteLogo from './assets/vite.svg';
 import heroImg from './assets/hero.png';
 import './App.css';
+import Greeting from './Greeting';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -25,6 +26,7 @@ function App() {
         >
           Count is {count}
         </button>
+        <Greeting />
       </section>
 
       <div className='ticks'></div>
---
+++ src/Greeting.jsx
@@ -0,0 +1,5 @@
+function Greeting() {
+  return <h1>This is my first React app</h1>;
+}
+
+export default Greeting;
